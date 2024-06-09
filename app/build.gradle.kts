01a96c56355d1ea8e1b884958768e8f15462df2d
Diogo Aluai <diogoaluai@gmail.com>
2024-06-10 00:51:26 +0100

    Improve jar name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,31 @@
 plugins {
-    java
+    id("java")
     `maven-publish`
 }
+
+var sdkBoostVersion = "1.0.0"
+var sdkJarName = "sdk-boost"
+
+java {
+    // Configuration to specify the jar name
+    withJavadocJar()
+    withSourcesJar()
+}
+
+tasks.jar {
+    manifest {
+        archiveBaseName.set(sdkJarName)
+        archiveVersion.set(sdkBoostVersion)
+    }
+}
+
 
 publishing {
     publications {
         create<MavenPublication>("maven") {
             groupId = "daluai.lib"
-            artifactId = "sdk-boost"
-            version = "1.0.0"
+            artifactId = sdkJarName
+            version = sdkBoostVersion
 
             from(components["java"])
         }
