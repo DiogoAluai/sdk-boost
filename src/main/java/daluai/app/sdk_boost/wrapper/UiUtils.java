7577df4c11e0aba2905e3d8631b1db5e72a3bfbb
Diogo Aluai <diogoaluai@gmail.com>
2024-08-18 15:28:38 +0100

    Add clear focus util

--- src/main/java/daluai/app/sdk_boost/wrapper/UiUtils.java
+++ src/main/java/daluai/app/sdk_boost/wrapper/UiUtils.java
@@ -1,21 +1,74 @@
 package daluai.app.sdk_boost.wrapper;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Rect;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
+/**
+ * Utility functions for UI.
+ */
 public final class UiUtils {
 
     private UiUtils() {
     }
 
+    /**
+     * Run callback on UI thread.
+     */
     public  static <T> void runCallbackOnMainThread(Runnable runnable) {
         new Handler(Looper.getMainLooper()).post(runnable);
     }
 
+
+    /**
+     * Run consumer callback on UI thread.
+     */
     public  static <T> void runCallbackOnMainThread(Consumer<T> callback, T data) {
         new Handler(Looper.getMainLooper()).post(() ->
                 callback.accept(data));
+    }
+
+    /**
+     * Run consumer callback on UI thread.
+     */
+    public  static <T> void runCallbackOnMainThread(Consumer<T> callback, Supplier<T> dataSupplier) {
+        new Handler(Looper.getMainLooper()).post(() ->
+                callback.accept(dataSupplier.get()));
+    }
+
+    /**
+     * Clear focus from components of given class, whenever user clicks away.
+     * Use it under the dispatchTouchEvent method:
+     * <pre>
+     * {@code
+     *      @Override
+     *      public boolean dispatchTouchEvent(MotionEvent event) {
+     *          UiUtils.clearFocusIfClickedAway(this, event)
+     *          return super.dispatchTouchEvent(event);
+     *      }
+     * }
+     * </pre>
+     */
+    public static void clearFocusIfClickedAway(Activity activity, MotionEvent event, Class<?> componentClass) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = activity.getCurrentFocus();
+            if (componentClass.isAssignableFrom(v.getClass())) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int) event.getRawX(), (int) event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
     }
 }
